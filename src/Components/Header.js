659f920157f8b7264eb383bd0aa01e5946e7e747
Rallo89 <nicolo.torreggiani@gmail.com>
2021-05-11 01:43:03 +0200

    Update header
    Modified Header, button layout and resumeData to retrieve the right CV

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     if (!this.props.data) return null;
 
     const project = this.props.data.project;
-    const github = this.props.data.github;
+    const curriculumvitae = this.props.data.curriculumvitae;
     const name = this.props.data.name;
     const description = this.props.data.description;
 
@@ -67,11 +67,8 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                <a href={curriculumvitae} className="button btn project-btn">
+                  <i className="fa fa-download"></i>Download CV
                 </a>
               </ul>
             </Fade>
